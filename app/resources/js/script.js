5d034e90bb33d07f5e4f15227a6005a50fe32467
Andrey Yatsevich <yatsevichao@gmail.com>
2017-11-20 16:58:45 +0400

    Валидация поля Count

--- app/resources/js/script.js
+++ app/resources/js/script.js
@@ -8,6 +8,21 @@
 
 $(document).ready(function () {
 
+    var product = [
+        {name: 'Товар 4', email: 'product1@mail.com', count: 5, price: '12'},
+        {name: 'Товар 55', email: 'product2@mail.com', count: 2, price: '120'},
+        {name: 'Товар 3', email: 'product3@mail.com', count: 7, price: '73'}];
+    var $table = $('#productList');
+    var $tbody = $('tbody', $table);
+    var dataNumber;
+    var modal;
+    var $btnAdd;
+    var $btnUpd;
+    var $nameProduct = $('#nameProduct');
+    var $requaredNameProduct = $('#requaredNameProduct');
+    var $count = $('#count');
+    var $requaredCount = $('#requaredCount');
+
     var validation = {
         'productName': function () {
             /*
@@ -35,48 +50,104 @@ $(document).ready(function () {
                             .toggleClass('visible', $('#nameProduct').val().length > 20)
                             .toggleClass('hidden', $('#nameProduct').val().length < 20);
             */
-            if (!$('#nameProduct').val().length) {
-                $('#requaredNameProduct').removeClass('hidden').addClass('visible');
+
+            if (!$nameProduct.val().length) {
+                $nameProduct
+                    .removeClass('input-success')
+                    .addClass('input-danger')
+                    .attr('data-correct', 'false');
+                //   $btnModal.addClass('disabled');
+                $requaredNameProduct
+                    .removeClass('hidden requared-success')
+                    .addClass('visible')
+                    .text('Поле обязательно для заполнения');
                 console.log('валидация сработала');
+            } else if ($nameProduct.val().length < 3 && $nameProduct.val().length) {
+                $nameProduct
+                    .removeClass('input-success')
+                    .addClass('input-danger')
+                    .attr('data-correct', 'false');
+                // $btnModal.addClass('disabled');
+                $requaredNameProduct
+                    .removeClass('hidden requared-success')
+                    .addClass('visible')
+                    .text('Минимальное количество символов 3');
+            } else if ($nameProduct.val().length > 20) {
+                $nameProduct
+                    .removeClass('input-success')
+                    .addClass('input-danger')
+                    .attr('data-correct', 'false');
+                // $btnModal.addClass('disabled');
+                $requaredNameProduct
+                    .removeClass('hidden requared-success')
+                    .addClass('visible')
+                    .text('Максимально количество символов 20');
+            } else if ($nameProduct.val().length >= 3 && $nameProduct.val().length <= 20) {
+                $nameProduct
+                    .removeClass('input-danger')
+                    .addClass('input-success')
+                    .attr('data-correct', 'true');
+                //$btnModal.removeClass('disabled');
+                $requaredNameProduct
+                    .text('Поле заполнено корректно')
+                    .removeClass('hidden')
+                    .addClass('visible requared-success');
             } else {
-                $('#requaredNameProduct').removeClass('visible').addClass('hidden');
-            }
 
-            if ($('#nameProduct').val().length < 3 && $('#nameProduct').val().length) {
-                $('#minRequaredNameProduct').removeClass('hidden').addClass('visible');
-            } else {
-                $('#minRequaredNameProduct').removeClass('visible').addClass('hidden');
-            }
-
-            if ($('#nameProduct').val().length > 20) {
-                $('#maxRequaredNameProduct').removeClass('hidden').addClass('visible');
-            } else {
-                $('#maxRequaredNameProduct').removeClass('visible').addClass('hidden');
-            }
-
-            if($('#nameProduct').val().length < 3 || $('#nameProduct').val().length > 20){
-                $('#nameProduct').addClass('input-danger');
-                $('#nameProduct').removeClass('input-success');
-                $('#requaredNameProductSuccess').removeClass('visible').addClass('hidden');
-            } else {
-                $('#nameProduct').removeClass('input-danger');
-                $('#nameProduct').addClass('input-success');
-                $('#requaredNameProductSuccess').removeClass('hidden').addClass('visible');
+                $nameProduct
+                    .removeClass('input-success')
+                    .addClass('input-danger')
+                    .attr('data-correct', 'false');
+                $requaredNameProduct
+                    .removeClass('visible')
+                    .addClass('hidden');
             }
             console.log('валидация попыталась сработать');
+        },
+        'count': function () {
+            var countNumber = Number($count.val());
+            if (!$count.val()) {
+                $count
+                    .removeClass('input-success')
+                    .addClass('input-danger')
+                    .attr('data-correct', 'false');
+                $requaredCount
+                    .text('Поле обязательно для заполнения')
+                    .removeClass('hidden requared-success')
+                    .addClass('visible requared');
+            } else if (!countNumber) {
+                $count
+                    .removeClass('input-success')
+                    .addClass('input-danger')
+                    .attr('data-correct', 'false');
+                $requaredCount
+                    .text('Должны быть введены только цифры')
+                    .removeClass('hidden requared-success')
+                    .addClass('visible requared');
+
+            } else {
+                $count
+                    .removeClass('input-danger')
+                    .addClass('input-success')
+                    .attr('data-correct', 'true');
+                $requaredCount
+                    .text('Поле заполнено корректно')
+                    .removeClass('hidden')
+                    .addClass('visible requared-success');
+            }
         }
     };
-    $('#nameProduct').change(validation.productName);
-    var product = [
-        {name: 'Товар 4', email: 'product1@mail.com', count: 5, price: '12'},
-        {name: 'Товар 55', email: 'product2@mail.com', count: 2, price: '120'},
-        {name: 'Товар 3', email: 'product3@mail.com', count: 7, price: '73'}];
-    var $table = $('#productList');
-    var $tbody = $('tbody', $table);
-    var dataNumber;
-    var modal;
-    var $btnAdd;
-    var $btnUpd;
+    $nameProduct.keyup(validation.productName).keyup(correct);
+    $count.keyup(validation.count).keyup(correct);
+
+    function correct() {
+        if ($nameProduct.attr('data-correct') == 'true' && $count.attr('data-correct') == 'true') {
+            $btnModal.removeClass('disabled');
+        } else {
+            $btnModal.addClass('disabled');
+        }
+    }
+
 
     function render(product) {
         $tbody.empty();
@@ -88,7 +159,7 @@ $(document).ready(function () {
                 $('<td></td>').text(product[i].name).append(
                     $('<span></span>').addClass('badge pull-right').text(product[i].count)
                 ),
-                $('<td></td>').text('$ ' + product[i].price),
+                $('<td></td>').text('$' + product[i].price),
                 $('<td></td>').append(
                     $('<button></button>').addClass('btn btn-success').text('Edit'),
                     $('<span> </span>'),
@@ -173,12 +244,23 @@ $(document).ready(function () {
         $overlay.css('display', 'block');
 
 
+
+
         dataNumber = $this.closest('tr').attr('data-number');
         $('#nameProduct').val(product[dataNumber].name);
         $('#email').val(product[dataNumber].email);
         $('#count').val(product[dataNumber].count);
         $('#price').val(product[dataNumber].price);
-
+        if ($count.val()) {
+            $count
+                .removeClass('input-danger')
+                .addClass('input-success')
+                .attr('data-correct', 'true');
+            $requaredCount
+                .text('Поле заполнено корректно')
+                .removeClass('hidden')
+                .addClass('visible requared-success');
+        }
 
         console.log('клик по кнопке добавить', product[dataNumber].name);
     });
@@ -190,6 +272,8 @@ $(document).ready(function () {
             $modalAdd.css('display', 'none');
             $modalDelete.css('display', 'none');
             $overlay.css('display', 'none');
+            $nameProduct.removeClass('input-success input-danger');
+            $requaredNameProduct.removeClass('visible').addClass('hidden');
         }
     });
 
@@ -225,51 +309,59 @@ $(document).ready(function () {
 
     $btnModal.click(function () {
         if ($btnModal.attr('data-id') == 'btnAdd') {
-            console.log('takoe');
             return addProduct();
 
         }
         if ($btnModal.attr('data-id') == 'btnUpd') {
-            console.log('takoe2');
             return updProduct();
         }
     });
-    //$btnUpd = $('#btnUpd');
-    //console.log($btnUpd);
-
-    //$btnUpd.click(updProduct);
 
     function addProduct() {
-        var newProduct = {};
-        newProduct.name = $('#nameProduct').val();
-        newProduct.email = $('#email').val();
-        newProduct.count = $('#count').val();
-        newProduct.price = $('#price').val();
+        if ($nameProduct.attr('data-correct') == 'true') {
 
-        console.log(newProduct);
+            var newProduct = {};
+            newProduct.name = $('#nameProduct').val();
+            newProduct.email = $('#email').val();
+            newProduct.count = $('#count').val();
+            newProduct.price = $('#price').val();
 
-        product.push(newProduct);
-        console.log(product);
-        $('html').css('overflow-y', 'auto');
-        $modalAdd.css('display', 'none');
-        $overlay.css('display', 'none');
-        render(product);
+            console.log(newProduct);
 
-        $('#addProductForm')[0].reset();
-        console.log('addProduct', $btnAdd)
+            product.push(newProduct);
+            console.log(product);
+            $('html').css('overflow-y', 'auto');
+            $nameProduct.removeClass('input-success input-danger');
+            $requaredNameProduct.removeClass('visible').addClass('hidden');
+            $modalAdd.css('display', 'none');
+            $overlay.css('display', 'none');
+            render(product);
+
+            $('#addProductForm')[0].reset();
+            console.log('addProduct', $btnAdd)
+        } else {
+            alert('Заполните корректно все обязательные поля');
+        }
+
     }
 
     function updProduct() {
-        product[dataNumber].name = $('#nameProduct').val();
-        product[dataNumber].email = $('#email').val();
-        product[dataNumber].count = $('#count').val();
-        product[dataNumber].price = $('#price').val();
+        if ($nameProduct.attr('data-correct') == 'true') {
+            product[dataNumber].name = $('#nameProduct').val();
+            product[dataNumber].email = $('#email').val();
+            product[dataNumber].count = $('#count').val();
+            product[dataNumber].price = $('#price').val();
 
-        render(product);
-        $('html').css('overflow-y', 'auto');
-        $modalAdd.css('display', 'none');
-        $overlay.css('display', 'none');
-        console.log('updProduct сработала', $btnUpd);
+            render(product);
+            $('html').css('overflow-y', 'auto');
+            $nameProduct.removeClass('input-success input-danger');
+            $requaredNameProduct.removeClass('visible').addClass('hidden');
+            $modalAdd.css('display', 'none');
+            $overlay.css('display', 'none');
+            console.log('updProduct сработала', $btnUpd);
+        } else {
+            alert('Заполните корректно все обязательные поля');
+        }
     }
 
     render(product);
